072b801d49fdebf5b9cd089ad4f38b407fdcb2cd
Daiane Ferreira <daisfcontato@gmail.com>
2024-10-27 19:15:17 -0300

    adicionando comentários

--- script.js
+++ script.js
@@ -7,27 +7,31 @@ function refresh() {
     console.log(numberToFind)
 }
 
-
 function verifyNumber() {
+    /*
+        Essa linha pega o elemento no html que tem o id igual a bet, que no caso é um input text
+        e acessa diretamente o valor que foi digitado pelo usuario dentro do input atraves do .value
+        e ensere esse valor na variavel bet
+    */
     let bet = document.getElementById('bet').value;
+
     if(bet > 100 || bet < 1) {
         alert('Aposta inválida')
+        // O return dentro de um if faz com que a funcao seja interrompida neste ponto.
         return;
     }
 
     if(bet > numberToFind) {
-
-        attemps++;
+        attemps = attemps + 1;
         alert('O número para ser encontrado é MENOR');
     }
     else if (bet < numberToFind) {
+        attemps = attemps + 1;
         alert('O número para ser encontrado é MAIOR');
     }
     else {
         alert('Parabéns você acertou! Com '+attemps+' erros')
     }
 }
-
-
 
 refresh();
